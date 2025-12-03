5db43a40c0eec9b37bf8c1e47cf221986f2f5ec5
Tima Minyaev <timatim333@inbox.ru>
2025-12-03 16:57:36 +0300

    fix: php-service fixed - /page/{slug}; /iss fixed

--- services/php-web/laravel-patches/routes/web.php
+++ services/php-web/laravel-patches/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AstroController;
+
 
 Route::get('/', fn() => redirect('/dashboard'));
 
 // Панели
 Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index']);
+Route::get('/iss',       [\App\Http\Controllers\IssController::class,       'index']);
 Route::get('/osdr',      [\App\Http\Controllers\OsdrController::class,      'index']);
 
 // Прокси к rust_iss
@@ -15,8 +18,6 @@ Route::get('/api/iss/trend', [\App\Http\Controllers\ProxyController::class, 'tre
 // JWST галерея (JSON)
 Route::get('/api/jwst/feed', [\App\Http\Controllers\DashboardController::class, 'jwstFeed']);
 Route::get("/api/astro/events", [\App\Http\Controllers\AstroController::class, "events"]);
-use App\Http\Controllers\AstroController;
-Route::get('/page/{slug}', [\App\Http\Controllers\CmsController::class, 'page']);
 Route::get('/page/{slug}', [\App\Http\Controllers\CmsController::class, 'page']);
 
 // CSV download
